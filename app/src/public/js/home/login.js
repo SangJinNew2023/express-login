071595a68f36ce50af4f37124cefffdcd93a6193
Sang jin An <photoan79@naver.com>
2023-05-21 23:11:59 -0700

    save HTML object with DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log("Hello");
-console.log("Hello1");
+
+const id = document.querySelector("#id"), //선택자 중복을 방지하기위해 #을 이용해 tag상의 id에 해당하는 정보를 가져옴
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button")
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+};
